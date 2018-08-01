9514373d5098a628ab106dc4710ed92fc1221971
Pranjal Singh <pranjals16@gmail.com>
2018-08-01 22:40:04 +0530

    Plot and SVM Code Added

--- src/ml_classifier.py
+++ src/ml_classifier.py
@@ -1,20 +1,26 @@
 import numpy as np
 import re
 
+from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import SGDClassifier, LassoCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_recall_curve, average_precision_score
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.externals import joblib
+from sklearn.svm import SVC, LinearSVC
 
 BASE_PATH = "../data/"
 
 
 def remove_hyperlink(text):
-    text = re.sub(r'http\S+', '', text)
+    text = re.sub(r"http\S+", '',  text.decode("utf-8"))
     return text
 
 
@@ -31,6 +37,19 @@ def read_files(file_type):
             y_list.append(int(line.strip()))
 
     return x_list, y_list
+
+
+def load_glove_model(glove_file):
+    print("Loading Glove Model")
+    f = open(glove_file, 'r')
+    model = {}
+    for line in f:
+        split_line = line.split()
+        word = split_line[0]
+        embedding = np.array([float(val) for val in split_line[1:]])
+        model[word] = embedding
+    print("Done.", len(model), " words loaded!")
+    return model
 
 
 def rf_random_search(x_train_tf_idf, y_train, x_test_tf_idf, y_test):
@@ -101,12 +120,56 @@ def mnb_grid_search(x_train_tf_idf, y_train, x_test_tf_idf, y_test):
     print("Multinomial Naive Bayes Grid Search: ", evaluate(best_grid, x_test_tf_idf, y_test))
 
 
+def plot_random(test_features, y_true):
+    mnb_clf = joblib.load('../model/mnb.pkl')
+    rf_clf = joblib.load('../model/rf.pkl')
+    adb_clf = joblib.load('../model/adb.pkl')
+    svc_linear_clf = joblib.load('../model/svc_linear.pkl')
+    svc_linera_lda_clf = joblib.load('../model/svc_lda.pkl')
+    y_proba_mnb = mnb_clf.predict_proba(test_features)[:, 1]
+    y_proba_rf = rf_clf.predict_proba(test_features)[:, 1]
+    y_proba_adb = adb_clf.predict_proba(test_features)[:, 1]
+    y_proba_svc_linear = svc_linear_clf.predict_proba(test_features)[:, 1]
+    y_proba_svc_linear_feat_select = svc_linera_lda_clf.predict_proba(test_features_red)[:, 1]
+    precision1, recall1, _ = precision_recall_curve(y_true, y_proba_mnb)
+    precision2, recall2, _ = precision_recall_curve(y_true, y_proba_rf)
+    precision3, recall3, _ = precision_recall_curve(y_true, y_proba_adb)
+    precision4, recall4, _ = precision_recall_curve(y_true, y_proba_svc_linear)
+    precision5, recall5, _ = precision_recall_curve(y_true, y_proba_svc_linear_feat_select)
+    plt.step(recall1, precision1, alpha=0.6, where='post', label="MNB")
+    plt.step(recall2, precision2, alpha=0.6, where='post', label="RF")
+    plt.step(recall3, precision3, alpha=0.6, where='post', label="ADB")
+    plt.step(recall4, precision4, alpha=0.6, where='post', label="SVC_Linear")
+    plt.step(recall5, precision5, alpha=0.6, where='post', label="SVC_Linearv2")
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.grid()
+    plt.legend()
+    plt.title('2-class Precision-Recall curve')
+    plt.show()
+
+
+def plot_pr_curve(y_true, y_proba):
+    precision, recall, _ = precision_recall_curve(y_true, y_proba)
+    plt.step(recall, precision, color='r', alpha=0.2,
+             where='post')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('2-class Precision-Recall curve')
+    plt.show()
+
+
 def evaluate(model, test_features, y_true):
     y_pred = model.predict(test_features)
     y_proba = model.predict_proba(test_features)[:, 1]
     accuracy = accuracy_score(y_true, y_pred) * 100.0
     auc_score = metrics.roc_auc_score(y_true, y_proba)
     precision_score = metrics.average_precision_score(y_true, y_proba)
+    plot_pr_curve(y_true, y_proba)
     return accuracy, auc_score, precision_score
 
 
@@ -127,23 +190,35 @@ def main():
     x_train_tf_idf = tf_idf_transformer.fit_transform(x_train_counts)
     x_test_tf_idf = tf_idf_transformer.transform(x_test_counts)
 
+    plot_random(x_test_tf_idf, y_test)
+    '''
+    svm_clf = SVC(probability=True, kernel='linear').fit(x_train_tf_idf, y_train)
+    accuracy, auc_score, precision_score = evaluate(svm_clf, x_test_tf_idf, y_test)
+    print("Linear SVM Metrics: Accuracy-{accuracy}, AUC-{auc_score} and PR_Score-{precision_score}"
+          .format(accuracy=accuracy, auc_score=auc_score, precision_score=precision_score))
+    joblib.dump(svm_clf, '../model/svc_linear.pkl')
+
     clf_nb = MultinomialNB(alpha=0.1, fit_prior=False).fit(x_train_tf_idf, y_train)
     accuracy, auc_score, precision_score = evaluate(clf_nb, x_test_tf_idf, y_test)
-    print("Multinomial Naive Bayes Metrics: Accuracy-{accuracy}, AUC-{auc_score} and PR_Score-{precision_score}"\
-        .format(accuracy=accuracy, auc_score=auc_score, precision_score=precision_score))
+    print("Multinomial Naive Bayes Metrics: Accuracy-{accuracy}, AUC-{auc_score} and PR_Score-{precision_score}"
+          .format(accuracy=accuracy, auc_score=auc_score, precision_score=precision_score))
+    joblib.dump(clf_nb, '../model/mnb.pkl')
+
     clf_rf = RandomForestClassifier(max_features='sqrt', min_samples_split=15, bootstrap=True,
                                     n_estimators=1000, max_depth=200).fit(x_train_tf_idf, y_train)
-
     accuracy, auc_score, precision_score = evaluate(clf_rf, x_test_tf_idf, y_test)
-    print("Random Forest Metrics: Accuracy-{accuracy}, AUC-{auc_score} and PR_Score-{precision_score}" \
-        .format(accuracy=accuracy, auc_score=auc_score, precision_score=precision_score))
+    print("Random Forest Metrics: Accuracy-{accuracy}, AUC-{auc_score} and PR_Score-{precision_score}"
+          .format(accuracy=accuracy, auc_score=auc_score, precision_score=precision_score))
+    joblib.dump(clf_rf, '../model/rf.pkl')
     # rf_random_search(x_train_tf_idf, y_train, x_test_tf_idf, y_test)
     # rf_grid_search(x_train_tf_idf, y_train, x_test_tf_idf, y_test)
     # mnb_grid_search(x_train_tf_idf, y_train, x_test_tf_idf, y_test)
     adb_clf = AdaBoostClassifier(n_estimators=500, learning_rate=0.2).fit(x_train_tf_idf, y_train)
     accuracy, auc_score, precision_score = evaluate(adb_clf, x_test_tf_idf, y_test)
-    print("AdaBoost Metrics: Accuracy-{accuracy}, AUC-{auc_score} and PR_Score-{precision_score}" \
-        .format(accuracy=accuracy, auc_score=auc_score, precision_score=precision_score))
+    print("AdaBoost Metrics: Accuracy-{accuracy}, AUC-{auc_score} and PR_Score-{precision_score}"
+          .format(accuracy=accuracy, auc_score=auc_score, precision_score=precision_score))
+    joblib.dump(adb_clf, '../model/adb.pkl')
+    '''
 
 
 if __name__ == "__main__":
